c16f524b745a3aafad71ef50d3fe9f16a2c01270
Robert Driskell <driskell.ba@gmail.com>
2019-07-21 20:57:08 -0500

    Clip test back to something that looks good

--- src/components/clip-path-test/ClipPath.jsx
+++ src/components/clip-path-test/ClipPath.jsx
@@ -7,10 +7,8 @@ const ClipPath = () => {
         <div className="clip-page">
             <div className="clip-container">
                 <div className="clip-inner">
-                    <div className="clip-fade-in">
-                        <h1>Clip Path Test</h1>
-                        <p>This is where I'd put all my cool information for this cool little card if I had any cool information to put in it, but I don't, because it's just a test, as you should know.</p>
-                    </div>
+                    <h1>Clip Path Test</h1>
+                    <p>This is where I'd put all my cool information for this cool little card if I had any cool information to put in it, but I don't, because it's just a test, as you should know.</p>
                 </div>
             </div>
         </div>
